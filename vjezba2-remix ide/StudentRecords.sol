bac18c25b81ad60874733a50240143e669f2dd2b
Randi Mohorović <randi.mohorovic@gmail.com>
2025-11-07 11:10:50 +0100

    vjezba3
    vjezba3

--- vjezba2-remix ide/StudentRecords.sol
+++ vjezba2-remix ide/StudentRecords.sol
@@ -6,11 +6,35 @@ contract StudentRecord {
         string name;
         uint grade;
     }
+    
+    address public owner;
+    constructor() payable {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "niste vlasnik");
+        _;
+    }
 
     mapping (uint => Student) public students;
     uint public count;
 
-    function addStudent(string memory _name, uint _grade) public { // _name udnerscore da nema konflikta izmedu imena ako se jos negdje koristi
+    //vjezba 3
+    mapping(address => uint) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function transfer(address _to, uint _amount) public {
+        require(balances[msg.sender] >= _amount, "nemate dovoljno sredstava");
+        balances[msg.sender] -= _amount;
+        balances[_to] += _amount;
+    }
+
+
+    function addStudent(string memory _name, uint _grade) public onlyOwner{ // _name udnerscore da nema konflikta izmedu imena ako se jos negdje koristi
         count++;
         students[count] = Student(_name, _grade);
     }
@@ -20,11 +44,17 @@ contract StudentRecord {
     return (s.name, s.grade);
     }
 
-    //vjezba
-    function updateGrade(uint _id, uint _grade) public {
+    function updateGrade(uint _id, uint _grade) public onlyOwner{
         require(_id > 0 && _id <= count, "student ne postoji");
         students[_id].grade = _grade;
 
     }
+    
+    function getBalance(address _user) public view returns (uint) {
+        return balances[_user];
+    }
+
+
+
 
 }
